9b5348399f686f9b933791251a571ab3d3b7d9e8
Noobster1214 <dkeanyew1214@gmail.com>
2023-12-18 09:32:39 +0800

    pagination for page list and page record list

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,94 +1,148 @@
 import React, { useEffect, useState } from "react";
 import { User } from "@auth0/auth0-spa-js";
-import Table from "react-bootstrap/Table";
-import Button from "react-bootstrap/Button";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import FormRecordList from "./components/form/FormRecordList";
 import { useAuth0 } from "@auth0/auth0-react";
-import fs from "fs";
+import { Col, Row, Button, Table } from "react-bootstrap";
 
 interface UserProps {
-    isAuthenticated: boolean;
-    user: User;
+  isAuthenticated: boolean;
+  user: User;
 }
 
 const Main = () => {
-    const { isAuthenticated, user } = useAuth0;
-	const [routesData, setRoutesData] = useState();
-    const navigate = useNavigate();
+  const { isAuthenticated, user } = useAuth0();
+  const [routesData, setRoutesData] = useState<any>();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
+  const navigate = useNavigate();
 
-    const handleViewClick = (url: string) => {
-        navigate(`/f/${url}`);
+  const handleViewClick = (url: string) => {
+    navigate(`/f/${url}`);
+  };
+
+  useEffect(() => {
+    const checkRoutesFile = async () => {
+      try {
+        const response = await fetch("./../routes.json");
+        const data = await response.json();
+        setRoutesData(data);
+      } catch (error) {
+        console.error("Error reading routes json: ", error);
+      }
     };
 
-	useEffect(() => {
-		const checkRoutesFile = async () => {
-			try {
-                const response = await fetch("./../routes.json");
-                const data = await response.json();
-                setRoutesData(data);
-            } catch (error) {
-                console.error("Error reading routes json: ", error);
-            }
-		};
+    checkRoutesFile();
+  }, []);
 
-		checkRoutesFile();
-	})
+  const totalPages = Math.ceil(
+    (routesData?.["form-list"]?.length || 0) / itemsPerPage
+  );
 
-    return (
-        <div>
-            <Table striped bordered responsive>
-                <thead
-                    style={{
-                        textAlign: "center",
-                        backgroundColor:
-                            process.env.REACT_APP_APPLICATION_THEME_COLOR,
-                    }}
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const visibleItems =
+    routesData?.["form-list"]?.slice(startIndex, endIndex) || [];
+
+  return (
+    <div>
+      <Row className="justify-content-end">
+        <Col xs="auto">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </Col>
+      </Row>
+      <Table striped bordered responsive>
+        <thead
+          style={{
+            textAlign: "center",
+            backgroundColor: process.env.REACT_APP_APPLICATION_THEME_COLOR,
+          }}
+        >
+          <tr>
+            <th style={{ width: "2%" }}>#</th>
+            <th style={{ width: "30%" }}>Page</th>
+            <th style={{ width: "2%" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {visibleItems.map((form, index) => (
+            <tr key={index}>
+              <td style={{ textAlign: "center" }}>{startIndex + index + 1}</td>
+              <td>{form.name}</td>
+              <td style={{ textAlign: "center" }}>
+                <Button
+                  variant="outline-light"
+                  style={{
+                    backgroundColor:
+                      process.env.REACT_APP_APPLICATION_THEME_COLOR,
+                  }}
+                  onClick={() => handleViewClick(form.url)}
                 >
-                    <tr>
-                        <th style={{ width: "2%" }}>#</th>
-                        <th style={{ width: "30%" }}>Page</th>
-                        <th style={{ width: "2%" }}>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {routesData &&
-                        routesData["form-list"].map((form, index) => (
-                            <tr key={index}>
-                                <td style={{ textAlign: "center" }}>
-                                    {index + 1}
-                                </td>
-                                <td>{form.name}</td>
-                                <td style={{ textAlign: "center" }}>
-                                    <Button
-                                        variant="outline-light"
-                                        style={{
-                                            backgroundColor:
-                                                process.env
-                                                    .REACT_APP_APPLICATION_THEME_COLOR,
-                                        }}
-                                        onClick={() =>
-                                            handleViewClick(form.url)
-                                        }
-                                    >
-                                        View
-                                    </Button>
-                                </td>
-                            </tr>
-                        ))}
-                </tbody>
-            </Table>
-        </div>
-    );
+                  View
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+const Pagination: React.FC<{
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (pageNumber: number) => void;
+}> = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="pagination">
+      <ul className="pagination-list list-unstyled d-flex">
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage - 1)}
+            className="page-link"
+            disabled={currentPage === 1}
+          >
+            {"<"}
+          </Button>
+        </li>
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage)}
+            className="page-link"
+            disabled
+          >
+            {`${currentPage}`}
+          </Button>
+        </li>
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage + 1)}
+            className="page-link"
+            disabled={currentPage === totalPages}
+          >
+            {">"}
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 const Home = () => {
-    // Use useNavigate to navigate user to other page
-    return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
 };
 
 export default Home;
--- client/src/components/form/FormRecordList.tsx
+++ client/src/components/form/FormRecordList.tsx
@@ -1,153 +1,227 @@
 import React, { useEffect, useState } from "react";
-import { Link, Route, Routes, useParams } from "react-router-dom";
+import { Link, Route, Routes, useParams, useNavigate } from "react-router-dom";
 import NewRecord from "./NewRecord";
 import ModifyRecord from "./ModifyRecord";
 import routesData from "./../../../routes.json";
 import { Row, Col, Table, Button } from "react-bootstrap";
 
 interface RouteProps {
-    name: string;
-    url: string;
-    table: string;
-    file: string;
+  name: string;
+  url: string;
+  table: string;
+  file: string;
 }
 
 interface RecordListProps {
-    tableName: string;
+  tableName: string;
 }
 
 const Main: React.FC<RecordListProps> = (props) => {
-    const { tableName } = props;
-    const [recordList, setRecordList] = useState();
+  const { tableName } = props;
+  const [recordList, setRecordList] = useState([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
+  const navigate = useNavigate();
 
-    const loadTableData = async () => {
-        try {
-            console.log(`Send request to backend`)
-            const response = await fetch(`/api/form/get-record-list/${tableName}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-
-            if (response.status === 200) {
-                const data = await response.json();
-                console.log(`Data`, data);
-                setRecordList(data.recordList);
-            }
-        } catch (error) {
-            console.error("Error loading data:", error);
+  const loadTableData = async () => {
+    try {
+      console.log(`Send request to backend`);
+      const response = await fetch(
+        `/api/form/get-record-list/${tableName}?page=${currentPage}&perPage=${itemsPerPage}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        return;
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log(`Data`, data);
+        setRecordList(data.recordList);
+      }
+    } catch (error) {
+      console.error("Error loading data:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadTableData();
+  }, [currentPage]);
+
+  const totalPages = Math.ceil(recordList.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const formatDate = (dateString) => {
+    const options = {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
     };
 
-    useEffect(() => {
-        loadTableData();
-    }, []);
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+      new Date(dateString)
+    );
+    return formattedDate;
+  };
 
-    
-    const formatDate = (dateString) => {
-        const options = {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-        };
-      
-        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
-        return formattedDate;
-      };
-    return (
-        <Row>
-            <Col>
-                <Link to={`./create-new`}>
-                    <Button
+  return (
+    <>
+      <Row className="justify-content-end">
+        <Col xs="auto">
+          <Link to={`./create-new`}>
+            <Button
+              variant="outline-light"
+              style={{
+                backgroundColor: process.env.REACT_APP_APPLICATION_THEME_COLOR,
+              }}
+            >
+              New Record
+            </Button>
+          </Link>
+        </Col>
+        <Col xs="auto">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Table striped bordered responsive className="mt-2 text-center">
+            <thead
+              style={{
+                backgroundColor: process.env.REACT_APP_APPLICATION_THEME_COLOR,
+              }}
+            >
+              <tr>
+                <th style={{ width: "30%" }}>ID</th>
+                <th style={{ width: "15%" }}>Date Created</th>
+                <th style={{ width: "2%" }}>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {recordList.map((record, index) => (
+                <tr key={index}>
+                  <td>{record.id}</td>
+                  <td>{formatDate(record.date_created)}</td>
+                  <td>
+                    <Link to={`./r/${record.id}`}>
+                      <Button
                         variant="outline-light"
                         style={{
-                            backgroundColor:
-                                process.env
-                                    .REACT_APP_APPLICATION_THEME_COLOR,
+                          backgroundColor:
+                            process.env.REACT_APP_APPLICATION_THEME_COLOR,
                         }}
-                    >
-                        New Record
-                    </Button>
-                </Link>
-                
-                <Table striped bordered responsive className="mt-2 text-center">
-                    <thead
-                        style={{
-                            backgroundColor:
-                                process.env.REACT_APP_APPLICATION_THEME_COLOR,
-                        }}
-                    >
-                        <tr>
-                            <th style={{ width: "30%" }}>ID</th>
-                            <th style={{ width: "15%" }}>Date Created</th>
-                            <th style={{ width: "2%" }}>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            recordList && recordList.map(record => (
-                                <tr key={record.id}>
-                                    <td>{record.id}</td>
-                                    <td>{formatDate(record.date_created)}</td>
-                                    <td>
-                                        <Link to={`./r/${record.id}`}>
-                                            <Button
-                                                variant="outline-light"
-                                                style={{
-                                                    backgroundColor:
-                                                        process.env
-                                                            .REACT_APP_APPLICATION_THEME_COLOR,
-                                                }}
-                                            >
-                                                View
-                                            </Button>
-                                        </Link>
-                                        
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </Table>
-            </Col>
-        </Row>
-    );
+                      >
+                        View
+                      </Button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+const Pagination: React.FC<{
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (pageNumber: number) => void;
+}> = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="pagination">
+      <ul className="pagination-list list-unstyled d-flex">
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage - 1)}
+            className="page-link"
+            disabled={currentPage === 1}
+          >
+            {"<"}
+          </Button>
+        </li>
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage)}
+            className="page-link"
+            disabled
+          >
+            {`${currentPage}`}
+          </Button>
+        </li>
+        <li className="page-item">
+          <Button
+            onClick={() => onPageChange(currentPage + 1)}
+            className="page-link"
+            disabled={currentPage === totalPages}
+          >
+            {">"}
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 const FormRecordList = () => {
-    const { form_list_url } = useParams();
-    const [routeProps, setRouteProps] = useState<RouteProps>();
+  const { form_list_url } = useParams();
+  const [routeProps, setRouteProps] = useState<RouteProps>();
 
-    useEffect(() => {
-        const similarForm = routesData["form-list"].find(
-            (form) => form.url === form_list_url
-        );
-        if (similarForm) {
-            console.log(similarForm);
-            setRouteProps({
-                name: similarForm.name,
-                url: similarForm.url,
-                table: similarForm.table,
-                file: similarForm.file
-            });
-        }
-    }, []);
-
-    return (
-        <Routes>
-            {routeProps && (
-                <>
-                    <Route path="/" element={<Main tableName={routeProps.table} />} />
-                    <Route path="/create-new" element={<NewRecord fileName={routeProps.file} tableName={routeProps.table} />} />
-                    <Route path="/r/:record_id" element={<ModifyRecord fileName={routeProps.file} tableName={routeProps.table} />} />
-                </>
-            )}
-        </Routes>
+  useEffect(() => {
+    const similarForm = routesData["form-list"].find(
+      (form) => form.url === form_list_url
     );
+    if (similarForm) {
+      console.log(similarForm);
+      setRouteProps({
+        name: similarForm.name,
+        url: similarForm.url,
+        table: similarForm.table,
+        file: similarForm.file,
+      });
+    }
+  }, []);
+
+  return (
+    <Routes>
+      {routeProps && (
+        <>
+          <Route path="/" element={<Main tableName={routeProps.table} />} />
+          <Route
+            path="/create-new"
+            element={
+              <NewRecord
+                fileName={routeProps.file}
+                tableName={routeProps.table}
+              />
+            }
+          />
+          <Route
+            path="/r/:record_id"
+            element={
+              <ModifyRecord
+                fileName={routeProps.file}
+                tableName={routeProps.table}
+              />
+            }
+          />
+        </>
+      )}
+    </Routes>
+  );
 };
 
 export default FormRecordList;
